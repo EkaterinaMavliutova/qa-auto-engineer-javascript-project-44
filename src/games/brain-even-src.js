c36033e0fa4d98f8c68aab3eea75c32d2ab574d2
ekaterinamavliutova <akumaaya@gmail.com>
2024-03-21 16:56:36 +0100

    refactor brain-even

--- src/games/brain-even-src.js
+++ src/games/brain-even-src.js
@@ -2,11 +2,16 @@ import { playGame, generateRandomInt } from '../index.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const checkIfEven = (num) => {
+  const isEven = num % 2 === 0;
+
+  return isEven ? 'yes' : 'no';
+};
+
 const generateGameQuestion = () => {
   const maxRandomInt = 100;
   const question = generateRandomInt(maxRandomInt);
-  const isEven = question % 2 === 0;
-  const answer = isEven ? 'yes' : 'no';
+  const answer = checkIfEven(question);
 
   return {
     question,
