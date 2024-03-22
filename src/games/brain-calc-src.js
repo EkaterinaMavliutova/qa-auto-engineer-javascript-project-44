37abc6b22ebdf57e4547abb360b94bcd56152278
ekaterinamavliutova <akumaaya@gmail.com>
2024-03-22 19:35:42 +0100

    refactor brain-calc

--- src/games/brain-calc-src.js
+++ src/games/brain-calc-src.js
@@ -1,31 +1,44 @@
 import { playGame, generateRandomInt } from '../index.js';
 
 const rule = 'What is the result of the expression?';
+const mathOperations = {
+  '+': (firstNum, secondNum) => firstNum + secondNum,
+  '-': (firstNum, secondNum) => firstNum - secondNum,
+  '*': (firstNum, secondNum) => firstNum * secondNum,
+};
 
-const generateMathOperator = () => {
-  const mathOperations = {
-    '+': (firstNum, secondNum) => firstNum + secondNum,
-    '-': (firstNum, secondNum) => firstNum - secondNum,
-    '*': (firstNum, secondNum) => firstNum * secondNum,
-  };
+const selectOperatorByIndex = (index) => {
   const operators = Object.keys(mathOperations);
-  const operator = operators[generateRandomInt(operators.length - 1)];
+
+  return operators[index];
+};
+
+const calculateExpression = (
+  operator,
+  firstNum,
+  secondNum,
+) => mathOperations[operator](firstNum, secondNum);
+
+const generateMathExpression = (firstNum, secondNum) => {
+  const opetatorsNum = Object.keys(mathOperations).length;
+  const operator = selectOperatorByIndex(generateRandomInt(opetatorsNum - 1));
+  const calculatedExpression = calculateExpression(operator, firstNum, secondNum);
 
   return {
     operator,
-    calculateExpression: mathOperations[operator],
+    calculatedExpression,
   };
 };
 
 const generateGameQuestion = () => {
-  const maxRandomInt = 100;
-  const firstArg = generateRandomInt(maxRandomInt);
-  const secondArg = generateRandomInt(maxRandomInt);
-  const { operator, calculateExpression } = generateMathOperator();
+  const maxArg = 100;
+  const firstArg = generateRandomInt(maxArg);
+  const secondArg = generateRandomInt(maxArg);
+  const { operator, calculatedExpression } = generateMathExpression(firstArg, secondArg);
 
   return {
     question: `${firstArg} ${operator} ${secondArg}`,
-    answer: calculateExpression(firstArg, secondArg),
+    answer: calculatedExpression,
   };
 };
 
